c46d95550691e37b78c965dfdb46a4abb1fc43fa
BlazeyBeats <43260036+BlazeyBeats@users.noreply.github.com>
2018-09-26 11:44:46 +0800

    Add files via upload

---
+++ week3.ino
@@ -0,0 +1,15 @@
+#include <Wire.h> 
+#include "SSD1306Wire.h" 
+SSD1306Wire  display(0x3c, 21, 22);
+void setup() {
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_24);
+}
+
+void loop() {
+  display.clear();
+  display.drawString(45,20,String(analogRead(A0)/16));
+  display.display();
+  delay(200);
+}
